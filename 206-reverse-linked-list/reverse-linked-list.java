632207b626f1911611643f4fd9d3ed0c2352d0eb
akshitap29 <akshitapaliwal44@gmail.com>
2026-07-15 18:12:51 +0530

    Time: 1 ms (4.49%) | Memory: 44.4 MB (30.94%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -11,6 +11,10 @@
 class Solution {
     public ListNode reverseList(ListNode head) 
     {
+        if(head==null || head.next==null)
+        {
+            return head;
+        }
         Stack<Integer> stack=new Stack<>();
         ListNode temp=head;
         while(temp!=null)
