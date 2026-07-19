1d78c14ab7a9e63f73527afc7fc151059be48c2e
akshitap29 <akshitapaliwal44@gmail.com>
2026-07-19 10:30:44 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (14.55%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.java
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -12,35 +12,51 @@ class MyQueue {
     
     public void push(int x) 
     {
-        while(!st1.isEmpty())
-        {
-            st2.push(st1.pop());
-        }
-        st1.push(x);
-        while(!st2.isEmpty())
-        {
-            st1.push(st2.pop());
-        }
-        
+       st1.push(x);
     }
     
     public int pop() 
     {
-        if(st1.isEmpty())
+        if(st2.isEmpty())
+        {
+            while(!st1.isEmpty())
+            {
+                st2.push(st1.pop());
+
+            }
+        }
+        
+        if(st2.isEmpty())
         {
             return -1;
         }
-        return st1.pop();
+        return st2.pop();
+        
+        
         
     }
     
     public int peek() 
     {
-        return st1.peek();    
+        if(st2.isEmpty())
+        {
+            while(!st1.isEmpty())
+            {
+                st2.push(st1.pop());
+
+            }
+        }
+       
+        if(st2.isEmpty())
+        {
+            return -1;
+        }
+        return st2.peek();
+         
     }
     
     public boolean empty() {
-        return st1.isEmpty();
+        return st1.isEmpty() && st2.isEmpty();
         
     }
 }
