0c0f9e07b5cb6cee171d163c1c7f06ef7cd9f086
akshitap29 <akshitapaliwal44@gmail.com>
2026-08-30 16:18:15 +0530

    Time: 1364 ms (99.97%) | Memory: 0B (100.00%) - LeetSync

--- 2057-count-salary-categories/count-salary-categories.sql
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -8,8 +8,8 @@ FROM(
     UNION ALL
     SELECT 'High Salary'
 )c
-LEFT JOIN(SELECT CASE
-
+LEFT JOIN(SELECT 
+        CASE
             WHEN income<20000 THEN 'Low Salary'
             WHEN income BETWEEN 20000 AND 50000 THEN 'Average Salary'
             ELSE 'High Salary'
